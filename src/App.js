56652307663399084d52e801f132db4b3dc04c1c
flows1314 <1778238738@qq.com>
2020-03-31 09:28:01 +0800

    ff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Input } from 'antd'
+import 'antd/dist/antd.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     hjdddd
+     <Button type='primary'>hhhhh</Button>
+     <Button type='danger'>hhhhh</Button>
+     <Button>hhhhh</Button>
+     <Input/>
+
     </div>
   );
 }
